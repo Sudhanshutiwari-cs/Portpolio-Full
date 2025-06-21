0f5edd676d2cf6ab09977f59fabf706a57a23889
Sudhanshu Tiwari <tiwarisudhanshu861@gmail.com>
2025-06-21 10:17:02 +0530

    REGULAR

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -100,7 +100,7 @@ const About = () => {
                   },
                   {
                     title: "GeeksforGeeks",
-                    description: "143 problems solved",
+                    description: "145 problems solved",
                     icon: <img src='images/gfg.png' width='35' height='35' alt='Leetcode icon' />
                   },
                   {
@@ -110,7 +110,7 @@ const About = () => {
                   },
                   {
                     title: "Coding Ninjas",
-                    description: "73 problems solved",
+                    description: "75 problems solved",
                     icon: <img src='images/coding.png' width='35' height='35' alt='Leetcode icon' />
                   }
                 ].map((value, index) => (
